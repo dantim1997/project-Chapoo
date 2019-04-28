c2ca4348c6482ea592418eabcedd855a6d2fab84
Tim Gras <t.gras.m@gmail.com>
2019-04-28 15:04:22 +0200

    added a DAO to check the dabase connection

--- project Chapoo/Form1.cs
+++ project Chapoo/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using project_Chapoo.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,10 @@ namespace project_Chapoo
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
