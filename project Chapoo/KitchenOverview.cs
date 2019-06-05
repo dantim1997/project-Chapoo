929aa9693d5cf02c1ee6bd7c6db83be5c084298e
dantim1997 <t.gras.m@gmail.com>
2019-06-05 14:23:43 +0200

    updated gerwns feedback

--- project Chapoo/KitchenOverview.cs
+++ project Chapoo/KitchenOverview.cs
@@ -82,7 +82,7 @@ namespace project_Chapoo
         public KitchenOverview(string typeOfView, Employee employee)
         {
             InitializeComponent();
-            TypeOfView = typeOfView;
+            TypeOfView = employee.TypeWorker;
             CurrentEmployee = employee;
             lbl_Loginname.Text = employee.Fullname;
             lbl_LoginID.Text = employee.EmployeeId.ToString();
@@ -91,33 +91,32 @@ namespace project_Chapoo
 
         private void Order(ChapooModels.Order order, int ordernumber)
         {
-            if (order != null)
+            if (order == null)
+                return;
+
+            switch (ordernumber)
             {
-                switch (ordernumber)
-                {
-                    case 1: order1 = order; break;
-                    case 2: order2 = order; break;
-                    case 3: order3 = order; break;
-                    case 4: order4 = order; break;
-                }
-                labels["name" + ordernumber].Text = employees.Where(p => p.EmployeeId == order.EmployeeId).FirstOrDefault().Fullname;
-                labels["table" + ordernumber].Text = order.TableNumber.ToString();
-                labels["time" + ordernumber].Text = order.Date.ToString("HH:mm"); ;
-                listviews["lv" + ordernumber].Items.Clear();
-                foreach (OrderProduct orderProduct in order.OrderProduct)
-                {
-                    ListViewItem item = new ListViewItem(orderProduct.OrderProductId.ToString());
-                    item.SubItems.Add(orderProduct.Product.ProductName);
-                    item.SubItems.Add(orderProduct.Amount.ToString());
-                    item.SubItems.Add((orderProduct.Status).ToString());
-                    listviews["lv" + ordernumber].Items.Add(item);
-                }
+                case 1: order1 = order; break;
+                case 2: order2 = order; break;
+                case 3: order3 = order; break;
+                case 4: order4 = order; break;
+            }
+            labels["name" + ordernumber].Text = employees.Where(p => p.EmployeeId == order.EmployeeId).FirstOrDefault().Fullname;
+            labels["table" + ordernumber].Text = order.TableNumber.ToString();
+            labels["time" + ordernumber].Text = order.Date.ToString("HH:mm"); ;
+            listviews["lv" + ordernumber].Items.Clear();
+            foreach (OrderProduct orderProduct in order.OrderProduct)
+            {
+                ListViewItem item = new ListViewItem(orderProduct.OrderProductId.ToString());
+                item.SubItems.Add(orderProduct.Product.ProductName);
+                item.SubItems.Add(orderProduct.Amount.ToString());
+                item.SubItems.Add((orderProduct.Status).ToString());
+                listviews["lv" + ordernumber].Items.Add(item);
             }
         }
 
         private void btn_Order1_Click(object sender, EventArgs e)
         {
-            Order_Service.UpdateStatus("Done", order1.OrderId);
             OrderProduct_Service.UpdateAllStatus(order1.OrderProduct, Statustype.Bereid);
             lv_Order1.Items.Clear();
             lb_NameOrder1.Text = string.Empty;
@@ -128,8 +127,7 @@ namespace project_Chapoo
 
         private void btn_Order2_Click(object sender, EventArgs e)
         {
-            Order_Service.UpdateStatus("Done", order2.OrderId);
-            OrderProduct_Service.UpdateAllStatus(order1.OrderProduct, Statustype.Bereid);
+            OrderProduct_Service.UpdateAllStatus(order2.OrderProduct, Statustype.Bereid);
             lv_Order2.Items.Clear();
             lb_NameOrder2.Text = string.Empty;
             lb_TableOrder2.Text = string.Empty;
@@ -139,8 +137,7 @@ namespace project_Chapoo
 
         private void btn_Order3_Click(object sender, EventArgs e)
         {
-            Order_Service.UpdateStatus("Done", order3.OrderId);
-            OrderProduct_Service.UpdateAllStatus(order1.OrderProduct, Statustype.Bereid);
+            OrderProduct_Service.UpdateAllStatus(order3.OrderProduct, Statustype.Bereid);
             lv_Order3.Items.Clear();
             lb_NameOrder3.Text = string.Empty;
             lb_TableOrder3.Text = string.Empty;
@@ -150,8 +147,7 @@ namespace project_Chapoo
 
         private void btn_Order4_Click(object sender, EventArgs e)
         {
-            Order_Service.UpdateStatus("Done", order4.OrderId);
-            OrderProduct_Service.UpdateAllStatus(order1.OrderProduct, Statustype.Bereid);
+            OrderProduct_Service.UpdateAllStatus(order4.OrderProduct, Statustype.Bereid);
             lv_Order4.Items.Clear();
             lb_NameOrder4.Text = string.Empty;
             lb_TableOrder4.Text = string.Empty;
@@ -164,10 +160,10 @@ namespace project_Chapoo
             List<ChapooModels.Order> newOrders = Order_Service.GetOrders(TypeOfView);
             foreach (ChapooModels.Order order in newOrders)
             {
-                if (Orders.Exists(t => t.OrderId == order.OrderId) == false && 
-                    order1.OrderId != order.OrderId && 
-                    order2.OrderId != order.OrderId && 
-                    order3.OrderId != order.OrderId && 
+                if (Orders.Exists(t => t.OrderId == order.OrderId) == false &&
+                    order1.OrderId != order.OrderId &&
+                    order2.OrderId != order.OrderId &&
+                    order3.OrderId != order.OrderId &&
                     order4.OrderId != order.OrderId)
                 {
                     Orders.Add(order);
